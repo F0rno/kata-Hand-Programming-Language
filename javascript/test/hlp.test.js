e1f46dccb95e92863291ac6a395fd349e4679bc1
F0rno <pablo.fornell.perinan@gmail.com>
2023-11-17 17:53:47 +0100

    Refactor memory read and write tests

--- javascript/test/hlp.test.js
+++ javascript/test/hlp.test.js
@@ -2,31 +2,33 @@ const { readMemoryAddress, increaseMemoryAddress, decreaseMemoryAddress } = requ
 const { describe, it, expect } = require('@jest/globals')
 
 describe('Hand Programing Language', function () {
-  it('should return 0 when I read a new memory address', function () {
-    const memory = new Map()
-    const result = readMemoryAddress(memory, 0)
-    expect(result).toBe(0)
-  })
-  it('should return the current value of the memory address plus 1 when we increase the value of the memory cell if is not 255', function () {
-    const memory = new Map()
-    const result = increaseMemoryAddress(memory, 0)
-    expect(result).toBe(1)
-  })
-  it('should return 0 if we increase a memory cell with a value of 255', function () {
-    const memory = new Map()
-    memory.set(0, 255)
-    const result = increaseMemoryAddress(memory, 0)
-    expect(result).toBe(0)
-  })
-  it('should return the current value of the memory address minus 1 when we decrease the value of the memory cell if is not 0', function () {
-    const memory = new Map()
-    increaseMemoryAddress(memory, 0)
-    const result = decreaseMemoryAddress(memory, 0)
-    expect(result).toBe(0)
-  })
-  it('should return 255 if we decrease a memory cell with value 0', function () {
-    const memory = new Map()
-    const result = decreaseMemoryAddress(memory, 0)
-    expect(result).toBe(255)
+  describe('Memory read and write', function () {
+    it('should return 0 when I read a new memory address', function () {
+      const memory = new Map()
+      const result = readMemoryAddress(memory, 0)
+      expect(result).toBe(0)
+    })
+    it('should return the current value of the memory address plus 1 when we increase the value of the memory cell if is not 255', function () {
+      const memory = new Map()
+      const result = increaseMemoryAddress(memory, 0)
+      expect(result).toBe(1)
+    })
+    it('should return 0 if we increase a memory cell with a value of 255', function () {
+      const memory = new Map()
+      memory.set(0, 255)
+      const result = increaseMemoryAddress(memory, 0)
+      expect(result).toBe(0)
+    })
+    it('should return the current value of the memory address minus 1 when we decrease the value of the memory cell if is not 0', function () {
+      const memory = new Map()
+      increaseMemoryAddress(memory, 0)
+      const result = decreaseMemoryAddress(memory, 0)
+      expect(result).toBe(0)
+    })
+    it('should return 255 if we decrease a memory cell with value 0', function () {
+      const memory = new Map()
+      const result = decreaseMemoryAddress(memory, 0)
+      expect(result).toBe(255)
+    })
   })
 })
