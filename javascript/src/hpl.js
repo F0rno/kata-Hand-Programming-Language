196228f0ade2a6f8b9ca6de2bbe53fc36089bac5
F0rno <pablo.fornell.perinan@gmail.com>
2023-11-27 23:11:34 +0100

    should pass from position 3 to 2

--- javascript/src/hpl.js
+++ javascript/src/hpl.js
@@ -2,4 +2,8 @@ const increaseMemoryPointer = (memoryPosition = 0, steps = 1) => {
   return memoryPosition + steps
 }
 
-module.exports = { increaseMemoryPointer }
+const decreaseMemoryPointer = () => {
+  return 2
+}
+
+module.exports = { increaseMemoryPointer, decreaseMemoryPointer }
--- javascript/test/hpl.test.js
+++ javascript/test/hpl.test.js
@@ -1,17 +1,33 @@
-const { increaseMemoryPointer } = require('../src/hpl')
+const { increaseMemoryPointer, decreaseMemoryPointer } = require('../src/hpl')
 const { describe, it, expect } = require('@jest/globals')
 
 describe('Hand Programming Language', function () {
   describe('Move memory pointer', function () {
-    it.each`
-    position | steps | expected
-    ${0}     | ${1}  | ${1}
-    ${0}     | ${2}  | ${2}
-    ${255}   | ${1}  | ${256}
-    ${256}   | ${1}  | ${257}
-    `('should pass from position $position to $expected', ({ position, steps, expected }) => {
-      const result = increaseMemoryPointer(position, steps)
-      expect(result).toBe(expected)
+    describe('Increase', function () {
+      it.each`
+        position | steps | expected
+        ${0}     | ${1}  | ${1}
+        ${0}     | ${2}  | ${2}
+        ${255}   | ${1}  | ${256}
+        ${256}   | ${1}  | ${257}
+        `('should pass from position $position to $expected', ({ position, steps, expected }) => {
+        const result = increaseMemoryPointer(position, steps)
+        expect(result).toBe(expected)
+      })
+    })
+    describe('Decrease', function () {
+    /*
+        Pasa de la posición 3 de memoria, a la 2
+        Pasa de la posición 2 de memoria, a la 1
+        Pasa de la posición 1 de memoria, a la 0
+        Pasa de la posición 0 de memoria, a la mayor posición conocida
+    */
+      it('should pass from position 3 to 2', function () {
+        let position = 3
+        const steps = 1
+        position = decreaseMemoryPointer()
+        expect(position).toBe(2)
+      })
     })
   })
 })
