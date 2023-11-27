58550278655d859fbfe157a45e277eeff9bf2531
F0rno <pablo.fornell.perinan@gmail.com>
2023-11-27 17:16:34 +0100

    should pass from position $position to $expected

--- javascript/test/hpl.test.js
+++ javascript/test/hpl.test.js
@@ -3,22 +3,15 @@ const { describe, it, expect } = require('@jest/globals')
 
 describe('Hand Programming Language', function () {
   describe('Move memory pointer', function () {
-    it('should pass from position 0 to 1', function () {
-      let position = 0
-      position = increaseMemoryPointer()
-      expect(position).toBe(1)
-    })
-    it('should pass from position 1 to 2', function () {
-      let position = 0
-      const steps = 2
-      position = increaseMemoryPointer(position, steps)
-      expect(position).toBe(2)
-    })
-    it('should pass from position 255 to 256', function () {
-      let position = 255
-      const steps = 1
-      position = increaseMemoryPointer(position, steps)
-      expect(position).toBe(256)
+    it.each`
+    position | steps | expected
+    ${0}     | ${1}  | ${1}
+    ${0}     | ${2}  | ${2}
+    ${255}   | ${1}  | ${256}
+    ${256}   | ${1}  | ${257}
+    `('should pass from position $position to $expected', ({ position, steps, expected }) => {
+      const result = increaseMemoryPointer(position, steps)
+      expect(result).toBe(expected)
     })
   })
 })
